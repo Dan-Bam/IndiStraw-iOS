24e6deefc7813e6fd6f42720ad30428c3e4e4113
Mindohyeon <min50875@naver.com>
2023-05-24 23:40:07 +0900

    Add logoLbael

--- Projects/Feature/RootFeature/Sources/RootViewController.swift
+++ Projects/Feature/RootFeature/Sources/RootViewController.swift
@@ -1,10 +1,12 @@
 import UIKit
 import BaseFeature
 import DesignSystem
+import Utility
 
 class RootViewController: BaseVC<RootViewModel> {
     private let logoLabel = UILabel().then {
         $0.text = "Indi Straw"
+        $0.textColor = .white
         $0.font = UIFont(font: DesignSystemFontFamily.Suit.bold, size: 30)
     }
     
@@ -13,9 +15,13 @@ class RootViewController: BaseVC<RootViewModel> {
     }
     
     override func addView() {
+        view.addSubviews(logoLabel)
     }
     
     override func setLayout() {
-        
+        logoLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(287)
+            $0.centerX.equalToSuperview()
+        }
     }
 }
