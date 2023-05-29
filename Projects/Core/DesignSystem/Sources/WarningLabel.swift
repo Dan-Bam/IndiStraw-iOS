e9dc5f2eaae721cf2ecd74f6ca61c8bc06fddf9c
Mindohyeon <min50875@naver.com>
2023-05-29 17:02:42 +0900

    Add WarningLabel

---
+++ Projects/Core/DesignSystem/Sources/WarningLabel.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+public class WarningLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        textColor = DesignSystemAsset.error.color
+        font = DesignSystemFontFamily.Suit.regular.font(size: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
