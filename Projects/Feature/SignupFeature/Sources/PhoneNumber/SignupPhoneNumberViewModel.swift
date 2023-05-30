49c94651d5e564c4b0b6a023f896d5008f750782
Mindohyeon <min50875@naver.com>
2023-05-30 20:55:56 +0900

    Add signupInputIDCoordinator 연동

--- Projects/Feature/SignupFeature/Sources/PhoneNumber/SignupPhoneNumberViewModel.swift
+++ Projects/Feature/SignupFeature/Sources/PhoneNumber/SignupPhoneNumberViewModel.swift
@@ -2,5 +2,7 @@ import Foundation
 import BaseFeature
 
 class SignupPhoneNumberViewModel: BaseViewModel {
-    
+    func pushProfileImageVC() {
+        coordinator.navigate(to: .selectPhotoIsRequired)
+    }
 }
