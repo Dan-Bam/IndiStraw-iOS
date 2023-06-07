83a4e7ca954da6d261818ee44d6824a823c0f30d
Mindohyeon <min50875@naver.com>
2023-06-07 13:30:49 +0900

    Add SigninRequest

---
+++ Projects/Feature/SigninFeature/Sources/Model/Signin.swift
@@ -0,0 +1,11 @@
+struct SigninRequest: Encodable {
+    var id: String
+    var password: String
+}
+
+//struct SigninResponse: Decodable {
+//    var accessToken: String
+//    var refreshToken: String
+//    var accessTokenExpiredAt: String
+//    var refreshTokenExpiredAt: String
+//}
