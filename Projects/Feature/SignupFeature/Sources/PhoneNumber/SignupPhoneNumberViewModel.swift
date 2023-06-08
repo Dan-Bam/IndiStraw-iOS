623c22fab996ec674f4c2aba8b2799ef505178ff
Mindohyeon <min50875@naver.com>
2023-06-08 10:26:25 +0900

    Update requestDuplicatePhoneNumber - responseDecodable -> responseData

--- Projects/Feature/SignupFeature/Sources/PhoneNumber/SignupPhoneNumberViewModel.swift
+++ Projects/Feature/SignupFeature/Sources/PhoneNumber/SignupPhoneNumberViewModel.swift
@@ -8,10 +8,11 @@ class SignupPhoneNumberViewModel: BaseViewModel {
     var container = DIContainer.shared.resolve(JwtStore.self)!
     
     func requestDuplicatePhoneNumber(phoneNumber: String, completion: @escaping (Result<Void, Error>) -> Void = { _ in }) {
-        AF.request(SignupTarget.sendAuthNumber(phoneNumber: phoneNumber))
-            .responseDecodable { (response: AFDataResponse<Data>) in
+        AF.request(SignupTarget.checkPhoneNumberDuplication(phoneNumber: phoneNumber))
+            .responseData { response in
                 switch response.result {
                 case .success:
+                    print("success")
                     completion(.success(()))
                 case .failure(let error):
                     completion(.failure(error))
