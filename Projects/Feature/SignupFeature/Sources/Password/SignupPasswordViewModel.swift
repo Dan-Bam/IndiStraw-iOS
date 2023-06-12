4bedfc5b0454a20d071e07c57b818f909a1445a9
Mindohyeon <min50875@naver.com>
2023-06-12 11:12:57 +0900

    Add requestToUploadImage

--- Projects/Feature/SignupFeature/Sources/Password/SignupPasswordViewModel.swift
+++ Projects/Feature/SignupFeature/Sources/Password/SignupPasswordViewModel.swift
@@ -1,8 +1,23 @@
-import Foundation
+import UIKit
 import BaseFeature
+import Alamofire
 
 class SignupPasswordViewModel: BaseViewModel {
     func popToRootVC() {
         coordinator.navigate(to: .popToRootIsRequired)
+    }
+    
+    
+    func requestToUploadImage(image: UIImage?) {
+        AF.upload(
+            multipartFormData: SignupTarget.uploadImage(image: image).multipart,
+            with: SignupTarget.uploadImage(image: image)).responseDecodable(of: ProfileImageModel.self) { [weak self] response in
+//                switch response.result {
+//                case .success(let data):
+//                    self?.pushInputIDVC()
+//                case .failure(let error):
+//                    print("Error - ImageUpload = \(error.localizedDescription)")
+//                }
+            }
     }
 }
