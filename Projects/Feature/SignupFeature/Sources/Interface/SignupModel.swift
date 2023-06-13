232730d1c9b6ea6ecbc94abeaa18aa4515303658
Mindohyeon <min50875@naver.com>
2023-06-13 15:48:30 +0900

    중복 파일 삭제

--- Projects/Feature/SignupFeature/Sources/Interface/SignupModel.swift
+++ Projects/Feature/SignupFeature/Sources/Interface/SignupModel.swift
@@ -6,12 +6,4 @@ struct SignupRequest: Encodable {
     var name: String
     var phoneNumber: String
     var profileUrl: String?
-    
-//    private init(id: String, password: String, name: String, phoneNumber: String, profileUrl: String? = nil) {
-//        self.id = id
-//        self.password = password
-//        self.name = name
-//        self.phoneNumber = phoneNumber
-//        self.profileUrl = profileUrl
-//    }
 }
