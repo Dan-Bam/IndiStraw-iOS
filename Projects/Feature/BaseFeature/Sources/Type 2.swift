30a85a8763f31c128eae9c798749e3b13eed008a
Mindohyeon <min50875@naver.com>
2023-06-15 10:12:13 +0900

    중복된 파일 제거

--- Projects/Feature/BaseFeature/Sources/Type 2.swift
+++
@@ -1,8 +0,0 @@
-public enum FindAccountType {
-    case findId, changePassword
-}
-
-public enum CheckPhoneDuplicateType {
-    public static let signup = "SIGNUP"
-    public static let password = "PASSWORD"
-}
--- Projects/Feature/SignupFeature/Sources/Interface/SignupModel 2.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct SignupRequest: Encodable {
-    var id: String
-    var password: String
-    var name: String
-    var phoneNumber: String
-    var profileUrl: String?
-}
