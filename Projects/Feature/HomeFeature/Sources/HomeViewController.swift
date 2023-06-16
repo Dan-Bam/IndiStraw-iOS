5b2c45ba6610ccec2c2797c5236e4bd3cd2bb540
Mindohyeon <min50875@naver.com>
2023-06-16 14:02:14 +0900

    Add Image - pagecontrolindicator

--- Projects/Feature/HomeFeature/Sources/HomeViewController.swift
+++ Projects/Feature/HomeFeature/Sources/HomeViewController.swift
@@ -1,6 +1,15 @@
 import UIKit
 import BaseFeature
+import SnapKit
+import Then
+
+var imageData: [String] = []
 
 class HomeViewController: BaseVC<HomeViewModel> {
     
+    private let pageControl = UIPageControl().then {
+        $0.setCurrentPageIndicatorImage(, forPage: <#T##Int#>)
+        $0.currentPage = 0
+        $0.numberOfPages = imageData.count
+    }
 }
