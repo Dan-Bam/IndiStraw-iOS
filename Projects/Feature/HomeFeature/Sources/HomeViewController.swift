94ac09a41d5fbd5c4c41f0ca216b92dc1b8b660e
Mindohyeon <min50875@naver.com>
2023-06-16 14:05:36 +0900

    Update targetVersion 15.0 -> 16.0

--- Projects/Feature/HomeFeature/Sources/HomeViewController.swift
+++ Projects/Feature/HomeFeature/Sources/HomeViewController.swift
@@ -2,13 +2,18 @@ import UIKit
 import BaseFeature
 import SnapKit
 import Then
+import DesignSystem
 
 var imageData: [String] = []
 
 class HomeViewController: BaseVC<HomeViewModel> {
     
     private let pageControl = UIPageControl().then {
-        $0.setCurrentPageIndicatorImage(, forPage: <#T##Int#>)
+        if #available(iOS 16.0, *) {
+            $0.setCurrentPageIndicatorImage(DesignSystemAsset.Images.pageControlIndicator.image, forPage: 2)
+        } else {
+            // Fallback on earlier versions
+        }
         $0.currentPage = 0
         $0.numberOfPages = imageData.count
     }
