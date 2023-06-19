d9807b1cc6c952b159c6f3c5a2dcd2283fe7c031
Mindohyeon <min50875@naver.com>
2023-06-19 09:16:56 +0900

    Add segCon

--- Projects/Feature/HomeFeature/Sources/HomeViewController 2.swift
+++ Projects/Feature/HomeFeature/Sources/HomeViewController 2.swift
@@ -12,10 +12,8 @@ var bannerImageSources = [
     DesignSystemAsset.Images.inputPhoto.image,
     UIImage(systemName: "check"),
     UIImage(systemName: "check")
-    
-]
 
-var segConArray = ["최근", "추천", "인기"]
+]
 
 class HomeViewController: BaseVC<HomeViewModel> {
     private let disposeBag = DisposeBag()
@@ -26,30 +24,12 @@ class HomeViewController: BaseVC<HomeViewModel> {
     }
     
     private let pageControl = UIPageControl().then {
-        $0.isUserInteractionEnabled = false
         $0.currentPage = 0
         $0.numberOfPages = bannerImageSources.count
         $0.setCurrentPageIndicatorImage(
             DesignSystemAsset.Images.pageControlIndicator.image,
             forPage: $0.currentPage
         )
-    }
-    
-    private let underLineView = UIView().then {
-        $0.tintColor = DesignSystemAsset.Colors.mainColor.color
-    }
-    
-    private let segCon = UISegmentedControl(items: segConArray).then {
-        $0.tintColor = DesignSystemAsset.Colors.
-    }
-    
-    private func removeBackgroundAndDidiver() {
-        let image = UIImage()
-        segCon.setBackgroundImage(image, for: .normal, barMetrics: .default)
-        segCon.setBackgroundImage(image, for: .selected, barMetrics: .default)
-        segCon.setBackgroundImage(image, for: .highlighted, barMetrics: .default)
-        
-        segCon.setDividerImage(image, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
     }
     
     private func setGesture() {
@@ -80,39 +60,14 @@ class HomeViewController: BaseVC<HomeViewModel> {
             }.disposed(by: disposeBag)
     }
     
-    @objc func segConChanged(_ segCon: UISegmentedControl) {
-        
-        
-    }
-    
-    override func viewDidLayoutSubviews() {
-        let underlineFinalXPosition = (self.segCon.bounds.width / CGFloat(self.segCon.numberOfSegments)) * CGFloat(self.segCon.selectedSegmentIndex)
-        UIView.animate(
-            withDuration: 0.1,
-            animations: {
-                self.underLineView.frame.origin.x = underlineFinalXPosition
-            }
-        )
-    }
-    
     override func configureVC() {
         navigationController?.navigationBar.prefersLargeTitles = false
         navigationItem.title = "hihi"
         setGesture()
-        
-        
-        segCon.rx.selectedSegmentIndex.changed
-            .bind(with: self) { owner, _ in
-                switch owner.segCon.selectedSegmentIndex {
-                    
-                default:
-                    return
-                }
-            }.disposed(by: disposeBag)
     }
     
     override func addView() {
-        view.addSubviews(bannerImageView, pageControl, segCon)
+        view.addSubviews(bannerImageView, pageControl)
     }
     
     override func setLayout() {
@@ -125,11 +80,6 @@ class HomeViewController: BaseVC<HomeViewModel> {
         pageControl.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(bannerImageView.snp.bottom).offset(16)
-        }
-        
-        segCon.snp.makeConstraints {
-            $0.top.equalTo(pageControl.snp.bottom).offset(20)
-            $0.leading.equalToSuperview().inset(15)
         }
     }
 }
