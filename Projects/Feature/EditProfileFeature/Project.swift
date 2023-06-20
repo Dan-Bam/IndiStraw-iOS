138b063246ae924f92d548d327bf3a496a981e8b
Mindohyeon <min50875@naver.com>
2023-06-20 09:23:59 +0900

    Add EditProfileFeature

--- Projects/Feature/EditProfileFeature/Project.swift
+++ Projects/Feature/EditProfileFeature/Project.swift
@@ -2,12 +2,10 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.makeModule(
-    name: "SettingFeature",
+    name: "EditProfileFeature",
     product: .staticLibrary,
     dependencies: [
         .Feature.BaseFeature,
-        .Feature.EditProfileFeature,
-        
         .Core.DesignSystem,
         .Domain.AuthDomain,
         .Shared.Utility,
--- Tuist/ProjectDescriptionHelpers/Dependency+Target.swift
+++ Tuist/ProjectDescriptionHelpers/Dependency+Target.swift
@@ -18,6 +18,7 @@ public extension TargetDependency.Feature {
     static let HomeFeature = TargetDependency.feature(name: "HomeFeature")
     static let ProfileFeature = TargetDependency.feature(name: "ProfileFeature")
     static let SettingFeature = TargetDependency.feature(name: "SettingFeature")
+    static let EditProfileFeature = TargetDependency.feature(name: "EditProfileFeature")
 }
 
 public extension TargetDependency.Core {
