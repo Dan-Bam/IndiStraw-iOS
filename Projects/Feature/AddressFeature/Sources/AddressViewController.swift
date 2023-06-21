f236901865d65402a007ab90e535655bc2538992
Mindohyeon <min50875@naver.com>
2023-06-21 17:24:52 +0900

    Add Extension - UITextField - addLeftPadding

--- Projects/Feature/AddressFeature/Sources/AddressViewController.swift
+++ Projects/Feature/AddressFeature/Sources/AddressViewController.swift
@@ -2,15 +2,40 @@ import UIKit
 import BaseFeature
 
 class AddressViewController: BaseVC<AddressViewModel> {
-    private let searchController = UISearchBar().then {
-        $0.showsCancelButton = false
-        $0.searchTextField.borderStyle = .none
-        $0.layer.borderWidth = 1
-        $0.layer.borderColor = UIColor.white.cgColor
-        $0.layer.cornerRadius = 5
+//    private let searchController = UISearchController().then {
+//        $0.searchBar.placeholder = "검색어를 입력해주세요."
+//        $0.searchBar.layer.borderWidth = 1
+//        $0.searchBar.layer.borderColor = UIColor.white.cgColor
+//        $0.searchBar.layer.cornerRadius = 5
+//    }
+    
+    private let searchTextField = UITextField().then {
         $0.placeholder = "검색어를 입력해주세요."
+        $0.textColor = .white
+        $0.layer.borderWidth = 1
+        $0.layer.cornerRadius = 5
+        $0.layer.borderColor = UIColor.white.cgColor
+        $0.attributedPlaceholder = NSAttributedString(
+            string: $0.placeholder!,
+            attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray])
     }
+    
     override func configureVC() {
-        navigationItem.titleView = searchController
+        navigationItem.titleView = searchTextField
+        navigationController?.isNavigationBarHidden = true
+//        navigationItem.titleView = searchController.searchBar
+    }
+    
+    override func addView() {
+        view.addSubviews(searchTextField)
+    }
+    
+    override func setLayout() {
+        searchTextField.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(55)
+            $0.trailing.equalToSuperview().inset(24)
+            $0.width.equalTo(308)
+            $0.height.equalTo(40)
+        }
     }
 }
---
+++ Projects/Feature/AddressFeature/Sources/Cell/AddressCell.swift
@@ -0,0 +1,9 @@
+//
+//  AddressCell.swift
+//  AddressFeature
+//
+//  Created by 민도현 on 2023/06/21.
+//  Copyright © 2023 dohyeon. All rights reserved.
+//
+
+import Foundation
