bdf891554af97598e12f6b5cec0d0d2eea37299b
Mindohyeon <min50875@naver.com>
2023-06-22 18:08:45 +0900

    Add requestToChangeAddress

--- Projects/Feature/AddressFeature/Sources/DetailAddress/DetailAddressViewModel.swift
+++ Projects/Feature/AddressFeature/Sources/DetailAddress/DetailAddressViewModel.swift
@@ -1,6 +1,11 @@
 import Foundation
 import BaseFeature
+import JwtStore
 
 class DetailAddressViewModel: BaseViewModel {
+    let container = DIContainer.shared.resolve(JwtStore.self)!
     
+    func requestToChangeAddress(data: ChangeAddressModel) {
+        
+    }
 }
