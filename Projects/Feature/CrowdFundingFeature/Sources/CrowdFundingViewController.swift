545b36cdb75fba2ba8133e32ca9ff6c67748daca
Mindohyeon <min50875@naver.com>
2023-07-03 08:56:44 +0900

    Add layout

--- Projects/Feature/CrowdFundingFeature/Sources/CrowdFundingViewController.swift
+++ Projects/Feature/CrowdFundingFeature/Sources/CrowdFundingViewController.swift
@@ -5,16 +5,21 @@ import Then
 import DesignSystem
 
 class CrowdFundingViewController: BaseVC<CrowdFundingViewModel> {
-    private let fundingImageView = UIImageView()
+    private let fundingImageView = UIImageView().then {
+        $0.backgroundColor = .gray
+    }
     
     private let mcLabel = UILabel().then {
+        $0.text = "진행자: 김성길"
         $0.textColor = DesignSystemAsset.Colors.lightGray.color
         $0.font = DesignSystemFontFamily.Suit.medium.font(size: 12)
     }
     
     private let fundingTitleLabel = UILabel().then {
+        $0.text = "그립톡으로 유기동물 보호하기"
         $0.textColor = .white
         $0.font = DesignSystemFontFamily.Suit.medium.font(size: 18)
+        $0.numberOfLines = 0
     }
     
     override func addView() {
@@ -29,6 +34,16 @@ class CrowdFundingViewController: BaseVC<CrowdFundingViewModel> {
             $0.top.equalTo(view.safeAreaLayoutGuide).inset(17)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(170)
+        }
+        
+        mcLabel.snp.makeConstraints {
+            $0.top.equalTo(fundingImageView.snp.bottom).offset(12)
+            $0.leading.equalToSuperview().inset(15)
+        }
+        
+        fundingTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(mcLabel.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview().inset(15)
         }
     }
 }
