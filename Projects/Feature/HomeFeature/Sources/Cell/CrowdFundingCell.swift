351b30ccf4a13947e0728cfff1c6c4fb3ee173c8
Mindohyeon <min50875@naver.com>
2023-07-04 12:11:42 +0900

    Update CrowdFundingCell Layout

--- Projects/Feature/HomeFeature/Sources/Cell/CrowdFundingCell.swift
+++ Projects/Feature/HomeFeature/Sources/Cell/CrowdFundingCell.swift
@@ -68,9 +68,14 @@ class CrowdFundingCell: UITableViewCell {
     }
     
     private func setLayout() {
+        fundingImageView.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview().inset(12)
+            $0.width.equalTo(120)
+        }
+        
         fundingTitleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(15)
-            $0.leading.equalToSuperview().inset(13)
+            $0.top.equalTo(fundingImageView)
+            $0.leading.equalTo(fundingImageView.snp.trailing).offset(12)
         }
 
         fundingDescriptionLabel.snp.makeConstraints {
@@ -78,22 +83,16 @@ class CrowdFundingCell: UITableViewCell {
             $0.leading.equalTo(fundingTitleLabel)
         }
 
-        fundingImageView.snp.makeConstraints {
-            $0.top.trailing.bottom.equalToSuperview().inset(8)
-            $0.leading.equalTo(fundingDescriptionLabel.snp.trailing).offset(-5)
-            $0.width.equalTo(120)
-        }
-
         fundingProgressView.snp.makeConstraints {
-            $0.top.equalTo(fundingDescriptionLabel.snp.bottom).offset(22)
+            $0.top.equalTo(fundingDescriptionLabel.snp.bottom).offset(38)
             $0.leading.equalTo(fundingTitleLabel)
-            $0.bottom.equalToSuperview().inset(8)
+            $0.bottom.equalToSuperview().inset(12)
             $0.height.equalTo(16)
         }
 
         fundingPercentageLabel.snp.makeConstraints {
             $0.leading.equalTo(fundingProgressView.snp.trailing).offset(5)
-            $0.trailing.equalTo(fundingImageView.snp.leading).offset(-14)
+            $0.trailing.equalToSuperview().inset(14)
             $0.centerY.equalTo(fundingProgressView)
         }
     }
