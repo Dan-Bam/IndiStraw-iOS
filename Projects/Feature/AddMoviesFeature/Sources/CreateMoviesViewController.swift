5de8c8aada7e7d4b051e8e269d02f0add6ab6b55
Mindohyeon <min50875@naver.com>
2023-07-13 17:16:23 +0900

    Add thumbnailTitleLabel, movieTitleLabel, subjectTitleLabel, descriptionTitleLabel

--- Projects/Feature/AddMoviesFeature/Sources/CreateMoviesViewController.swift
+++ Projects/Feature/AddMoviesFeature/Sources/CreateMoviesViewController.swift
@@ -1,6 +1,31 @@
 import UIKit
 import BaseFeature
+import SnapKit
+import Then
+import DesignSystem
 
 class CreateMoviesViewController: BaseVC<CreateMoviesViewModel> {
+    private let thumbnailTitleLabel = UILabel().then {
+        $0.text = "썸네일"
+        $0.textColor = .white
+        $0.font = DesignSystemFontFamily.Suit.regular.font(size: 16)
+    }
     
+    private let movieTitleLabel = UILabel().then {
+        $0.text = "영화"
+        $0.textColor = .white
+        $0.font = DesignSystemFontFamily.Suit.regular.font(size: 16)
+    }
+    
+    private let subjectTitleLabel = UILabel().then {
+        $0.text = "제목"
+        $0.textColor = .white
+        $0.font = DesignSystemFontFamily.Suit.regular.font(size: 16)
+    }
+    
+    private let descriptionTitleLabel = UILabel().then {
+        $0.text = "소개"
+        $0.textColor = .white
+        $0.font = DesignSystemFontFamily.Suit.regular.font(size: 16)
+    }
 }
